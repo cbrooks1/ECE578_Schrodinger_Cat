6f92c77069042e17a597824773e50351b1248087
rickii-fox <erikfox589@gmail.com>
2018-12-07 12:45:04 -0800

    Update directorNode.py

--- Software/ROS/directorNode.py
+++ Software/ROS/directorNode.py
@@ -6,38 +6,36 @@ from std_msgs.msg import Float32
 from std_msgs.msg import String
 
 #initialize Global Variables
-face_command=
-image_command=
+#face_command=
+#image_command=
 motion_command='Stop'
 #mfeedback_command=
 
 #Callback funtions
-def callback1(data):
-def callback2(data):
+#def callback1(data):
+#def callback2(data):
 
 
 #Publisher Nodes
-def face_talker():
-	global face_command
+#def face_talker():
+#	global face_command
 	#set up node as publisher
-	pub=rospy.Publisher('face',Int32,queue_size=10)
-
+#	pub=rospy.Publisher('face',Int32,queue_size=10)
 	
 	#publish next command
-	rate=rospy.Rate(.2)
-	pub.publish(face_command)
-	rate.sleep()
+#	rate=rospy.Rate(.2)
+#	pub.publish(face_command)
+#	rate.sleep()
 	
-def image_talker():
-	global image_command
+#def image_talker():
+#	global image_command
 	#set up node as publisher
-	pub=rospy.Publisher('image_topic',Image??,queue_size=10)
+#	pub=rospy.Publisher('image_topic',Image??,queue_size=10)
 
-	
 	#publish next command
-	rate=rospy.Rate(.2)
-	pub.publish(image_command)
-	rate.sleep()
+#	rate=rospy.Rate(.2)
+#	pub.publish(image_command)
+#	rate.sleep()
 
 def mcommand_talker():
 	global motion_command
@@ -55,28 +53,25 @@ def mcommand_talker():
 	#set up node as publisher
 #	pub=rospy.Publisher('motion_feedback',String,queue_size=10)
 
-	
 	#publish next command
 #	rate=rospy.Rate(.2)
 #	pub.publish(mfeedback_command)
 #	rate.sleep()
 
-	
-#suscriber nodes: Do we need this? Can we just use a director function and publish what we want to happen
-def listener():
+#def talker():
 	#Set up node
-	rospy.init_node('director', anonymous=True)
+#	rospy.init_node('talker', anonymous=True)
 	
 	#suscribe node to the sensor's topics
-	rospy.Subscriber('coordinates',Float32, callback1) 
-	rospy.Subscriber('/turtle1/cmd_vel',Twist??, callback2)
-	rospy.spin()
+#	rospy.Subscriber('coordinates',Float32, callback1) 
+#	rospy.spin()
 
 #Main: Play Director fuction: controls the actions of our robot in correct 
 def director():
+	rospy.init_node('director', anonymous=True)
 	
 if __name__=='__main__':
 	try:
-		listener()
+		director()
 	except rospy.ROSInterruptException:
 		pass
