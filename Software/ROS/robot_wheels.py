54d6037141e7578f3cc0ecc7ec65ed8e58011dc6
dkoppad <44821084+dkoppad@users.noreply.github.com>
2018-12-07 13:08:39 -0800

    Add files via upload

--- Software/ROS/robot_wheels.py
+++ Software/ROS/robot_wheels.py
@@ -3,56 +3,70 @@
 import rospy
 from std_msgs.msg import Int32
 from std_msgs.msg import String
-import time
+from time import sleep
 import sys
-from pyfirmata import Arduino, util
-board = Arduino('/dev/ttyUSB0')
+
+from gpiozero import pin12
+from gpiozero import pin9
+from gpiozero import pin8
+from gpiozero import pin7
+from gpiozero import pin6
+from gpiozero import pin5
+
+p12 = pin12(12)
+p9 = pin9(9)
+p8 = pin8(8)
+p7 = pin7(7)
+p6 = pin6(6)
+p5 = pin5(5)
 
 global motion_command
 global pubMotion_Command
 global motion_out
-motion_commandList = ["Go_Forward","Go_Backward","Turn_Left","Turn_Right","Stop"]
+motion_commandList = ["Go_Forward","Go_Backward","Turn_Left","Turn_Right","Stop","Demo"]
 
 #pubmotion = rospy.Publisher('motion_out',Int32, queue_size=1)
 
 def callback0(data):
     motion_out = data.data
-    
 
-def callback1(data):
-    feedback = data.data
 
 def robot_wheels():
     rospy.init_node("robot_wheels", anonymous=True)
     rate = rospy.Rate(10) # 10Hz
     rospy.Subscriber("motion_command", String, callback0)
-    rospy.Subscriber("motion_feedback", String, callback1)
+    #rospy.Subscriber("motion_feedback", String, callback1)
 
     if motion_command in motion_commandList:
         if motion_command == "Go_Forward":
-            pin2 = board.getpin('d:2:o')
-            pin2.write(1)
+            p12.on()
+            sleep(1)
             print ("-I am moving forward")
         
         if motion_command == "Go_Backward":
-            pin3 = board.getpin('d:3:o')
-            pin3.write(1)
+            p9.on()
+            sleep(1)
             print ("-I am moving forward")
         
         if motion_command == "Turn_left":
-            pin4 = board.getpin('d:4:o')
-            pin4.write(1)
+            p8.on()
+            sleep(1)
             print ("-I am Turning Let")
         
         if motion_command == "Turn_right":
-            pin5 = board.getpin('d:5:o')
-            pin5.write(1)
+            p7.on()
+            sleep(1)
             print ("-I am Turning right")
         
         if motion_command == "Stop":
-            pin6 = board.getpin('d:6:o')
-            pin6.write(1)
+            p6.on()
+            sleep(1)
             print ("-I stopped")
+        
+        if motion_command == "Demo":
+            p5.on()
+            sleep(1)
+            print ("Demo mode is on")
         else:
             print ("unknown command!!")
                 
