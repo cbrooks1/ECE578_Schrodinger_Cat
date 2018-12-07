8d7ed049086009128631c725f1f87aeefa287a25
dkoppad <44821084+dkoppad@users.noreply.github.com>
2018-12-07 11:15:42 -0800

    Add files via upload

--- Software/ROS/robot_wheels.py
+++ Software/ROS/robot_wheels.py
@@ -11,15 +11,15 @@ board = Arduino('/dev/ttyUSB0')
 global motion_command
 global pubMotion_Command
 global motion_out
-motion_commandList = ["Go_Forward","Turn_Left","Turn_Right","Stop"]
+motion_commandList = ["Go_Forward","Go_Backward","Turn_Left","Turn_Right","Stop"]
 
 #pubmotion = rospy.Publisher('motion_out',Int32, queue_size=1)
 
-def callback0(data)
+def callback0(data):
     motion_out = data.data
     
 
-def callback1(data)
+def callback1(data):
     feedback = data.data
 
 def robot_wheels():
@@ -44,17 +44,19 @@ def robot_wheels():
             pin4.write(1)
             print ("-I am Turning Let")
         
-        if motion_command == "Turn_right"
+        if motion_command == "Turn_right":
             pin5 = board.getpin('d:5:o')
             pin5.write(1)
             print ("-I am Turning right")
         
-        if motion_command == "Stop"
+        if motion_command == "Stop":
             pin6 = board.getpin('d:6:o')
             pin6.write(1)
             print ("-I stopped")
         else:
-            print ("unknown command!!)
+            print ("unknown command!!")
+                
+    rospy.spin()
     """
     while not rospy.is_shutdown():
         pubmotion.publish(motion_out)
