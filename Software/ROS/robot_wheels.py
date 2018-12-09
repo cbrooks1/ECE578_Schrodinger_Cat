ea1e4ca59b8e0215847d469c7ff2cf7bdd2143d1
rickii-fox <erikfox589@gmail.com>
2018-12-09 00:32:52 +0000

    Update robot_wheels.py

--- Software/ROS/robot_wheels.py
+++ Software/ROS/robot_wheels.py
@@ -1,103 +1,82 @@
+#!/usr/bin/env python
 import rospy
 from std_msgs.msg import Int32
 from std_msgs.msg import String
 from time import sleep
 import sys
+import serial
 
-from gpiozero import LED as gpiopin
-#from gpiozero import pin9
-#from gpiozero import pin8
-#from gpiozero import pin7
-#from gpiozero import pin6
-#from gpiozero import pin5
+ser = serial.Serial("/dev/serial/by-id/usb-Arduino_Srl_Arduino_Uno_75330303035351715080-if00", baudrate=9600, timeout=3.0)
 
-p12 = gpiopin(12)
-p9 = gpiopin(9)
-p8 = gpiopin(8)
-p7 = gpiopin(7)
-p6 = gpiopin(6)
-p5 = gpiopin(5)
 
-global motion_command
-global pubMotion_Command
-global motion_out
-motion_commandList = ["Go_Forward","Go_Backward","Turn_Left","Turn_Right","Stop","Demo"]
-
+motion_cmd='Stop'
+#motion_cmd = "Go_Forward"
+motion= "Stop"
+#motion_commandList = ["Go_Forward","Go_Backward","Turn_Left","Turn_Right","Stop","Demo"]
+Flag=0
 #pubmotion = rospy.Publisher('motion_out',Int32, queue_size=1)
 
 def callback0(data):
-    motion_command = data.data
+    global motion
+    motion = data.data
+    print('callback')
+    navigation()
+    #print(motion)
 
+def navigation():
+    global motion_cmd, motion
+    motion_cmd = motion
+    print("insde navigation" ,motion_cmd)
+    if (motion_cmd == "Go_Forward"):
+        print("FFFF")
+        ser.write('f'+'\n')
+        #rospy.sleep(1)
+        print ("-I am moving forward")
+            
+            
+    elif (motion_cmd == "Go_Backward"):
 
+        print("BBBB")
+        ser.write('b'+'\n')
+        #rospy.sleep(1)
+        print ("-I am moving backward")
+            
+    elif (motion_cmd == "Turn_Right"):
+        print ("RRR")
+        ser.write('r'+'\n')
+        #rospy.sleep(1)
+        print ("-I am Turning Right")
+            
+    elif (motion_cmd == "Turn_Left"):
+        print("LLLL")
+        ser.write('l'+'\n')
+        #rospy.sleep(1)
+        print ("-I am Turning left")
+            
+    elif (motion_cmd =="Stop"):
+        print("SSSS")
+        ser.write('s'+'\n')
+        #rospy.sleep(1)
+        print ("-I stopped")
+            
+    elif (motion_cmd =="Demo"):
+        ser.write('d'+'\n')
+        #rospy.sleep(1)
+        print ("Demo mode is on")
+    else:
+        print ("unknown command!!")
+        
+       
+        rospy.spin()
+    
 def robot_wheels():
+    global motion_cmd , motion
     rospy.init_node("robot_wheels", anonymous=True)
-    rate = rospy.Rate(10) # 10Hz
-    rospy.Subscriber("motion_command", String, callback0)
+ #   rate = rospy.Rate(10) # 10Hz
+    print('robot wheels')
+    rospy.Subscriber("/motion_command",String, callback0)
     #rospy.Subscriber("motion_feedback", String, callback1)
 
-    if motion_command in motion_commandList:
-        if motion_command == "Go_Forward":
-            p5.off()
-            p6.off()
-            p7.off()
-            p8.off()
-            p9.off()
-            p12.on()
-            sleep(1)
-            print ("-I am moving forward")
-        
-        if motion_command == "Go_Backward":
-            p5.off()
-            p6.off()
-            p7.off()
-            p8.off()
-            p12.off()
-            p9.on()
-            sleep(1)
-            print ("-I am moving forward")
-        
-        if motion_command == "Turn_left":
-            p5.off()
-            p6.off()
-            p7.off()
-            p9.off()
-            p12.off()
-            p8.on()
-            sleep(1)
-            print ("-I am Turning Let")
-        
-        if motion_command == "Turn_right":
-            p5.off()
-            p6.off()
-            p8.off()
-            p9.off()
-            p12.off()
-            p7.on()
-            sleep(1)
-            print ("-I am Turning right")
-        
-        if motion_command == "Stop":
-            p5.off()
-            p7.off()
-            p8.off()
-            p9.off()
-            p12.off()
-            p6.on()
-            sleep(1)
-            print ("-I stopped")
-        
-        if motion_command == "Demo":
-            p5.on()
-            p6.off()
-            p7.off()
-            p8.off()
-            p9.off()
-            p12.off()
-            sleep(1)
-            print ("Demo mode is on")
-        else:
-            print ("unknown command!!")
-                
     rospy.spin()
     """
     while not rospy.is_shutdown():
@@ -108,6 +87,8 @@ def robot_wheels():
 if __name__ == '__main__':
 	print("Schrodingers Cat")
 	try:
-		robot_wheels()
+            
+                robot_wheels()
+		
 	except rospy.ROSInterruptException():
 		pass
