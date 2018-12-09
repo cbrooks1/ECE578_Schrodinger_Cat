eca914689a57bcebfb8851ba4dd0c4fa89d9526a
Jamie W <jamiekwi@gmail.com>
2018-12-08 22:51:37 -0800

    Update servo node

--- Software/ROS/servo_control.py
+++ Software/ROS/servo_control.py
@@ -5,97 +5,87 @@ from std_msgs.msg import Int32
 from std_msgs.msg import String
 from time import sleep
 import sys
+import serial
 
-from gpiozero import LED as gpioPin
+ser = serial.Serial("/dev/serial/by-id/usb-Arduino_Srl_Arduino_Uno_75330303035351715080-if00", baudrate=9600, timeout=3.0)
 
-pin21 = gpioPin(21)
-pin20 = gpioPin(20)
-pin16 = gpioPin(16)
-pin26 = gpioPin(26)
-pin19 = gpioPin(19)
-pin13 = gpioPin(13)
 
-global servo_command
-global pubServo_Command
+servo_command = "Neutral" # Default command
+servo_cmd = "Neutral" # Default command
 global servo_out
 servo_commandList = ["Wave","Point","No","Neutral","Arms_up","Demo"]
 
 # Here is the publisher for servo commands:
 #pubservo = rospy.Publisher('servo_command', String, queue_size=1)
 
-def callback0(data):
-    servo_out = data.data  
+def callback_servo(data):
+	global servo_command
+    servo_command = data.data
+	print('Callback -')
+    run_command()
+    #print(servo_command)	
 
+def run_command():
+    global servo_cmd, servo_command
+    servo_cmd = servo_command
+    print("Inside run command, running: " ,servo_cmd)
+	
+    if (servo_cmd == "Wave"):
+        print("Wave start")
+        ser.write('w'+'\n')
+        #rospy.sleep(1)
+        print ("-I am waving")
+            
+            
+    elif (servo_cmd == "Point"):
+        print("Point start")
+        ser.write('p'+'\n')
+        #rospy.sleep(1)
+        print ("-I am pointing")
+            
+    elif (servo_cmd == "No"):
+        print ("No start")
+        ser.write('n'+'\n')
+        #rospy.sleep(1)
+        print ("-I am gesturing no")
+            
+    elif (servo_cmd == "Neutral"):
+        print("Neutral start")
+        ser.write('x'+'\n')
+        #rospy.sleep(1)
+        print ("-I am returning to neutral")
+            
+    elif (servo_cmd =="Arms_up"):
+        print("Arms up start")
+        ser.write('a'+'\n')
+        #rospy.sleep(1)
+        print ("-I am raising my arms")
+            
+    elif (servo_cmd =="Demo"):
+        ser.write('m'+'\n')
+        #rospy.sleep(1)
+        print ("Demo mode is on")
+    else:
+        print ("Invalid command - no actions")
+        
+       
+        rospy.spin()
 
 def servo_control():
+    global servo_cmd , servo_command
     rospy.init_node("servo_control", anonymous=True)
-    rate = rospy.Rate(10) # 10Hz
-    rospy.Subscriber("servo_command", String, callback0)
+ #   rate = rospy.Rate(10) # 10Hz
+    print('Running node: Servo control')
+    rospy.Subscriber("/servo_control",String, callback_servo)
 
-    if servo_command in servo_commandList:
-        if servo_command == "Wave":
-            pin21.on()
-			pin20.off()
-			pin16.off()
-			pin26.off()
-			pin19.off()
-			pin13.off()
-            print ("-I am waving")
-        
-        if servo_command == "Point":
-            pin21.off()
-			pin20.on()
-			pin16.off()
-			pin26.off()
-			pin19.off()
-			pin13.off()
-            print ("-I am pointing")
-        
-        if servo_command == "No":
-            pin21.off()
-			pin20.off()
-			pin16.on()
-			pin26.off()
-			pin19.off()
-			pin13.off()
-            print ("-I am gesturing negatively")
-        
-        if servo_command == "Neutral":
-            pin21.off()
-			pin20.off()
-			pin16.off()
-			pin26.on()
-			pin19.off()
-			pin13.off()
-            print ("-I am returning to neutral position")
-        
-        if servo_command == "Arms_up":
-            pin21.off()
-			pin20.off()
-			pin16.off()
-			pin26.off()
-			pin19.on()
-			pin13.off()
-            print ("-I raising my arms")
-			
-		if servo_command == "Demo":
-            pin21.off()
-			pin20.off()
-			pin16.off()
-			pin26.off()
-			pin19.off()
-			pin13.on()
-            print ("-I will run a demo of my actions")
-        else:
-			pin21.off()
-			pin20.off()
-			pin16.off()
-			pin26.off()
-			pin19.off()
-			pin13.off()
-            print ("unknown command!!")
-                
     rospy.spin()
+    """
+    while not rospy.is_shutdown():
+        pubmotion.publish(servo_out)
+    rate.sleep()
+    rospy.spin()
+    """
+
 
 	
 if __name__ == '__main__':
